4ecb8d21fcebe6c974deeb82230b2c0f327b1791
simmimak <34087428+simmimak@users.noreply.github.com>
2023-04-15 18:38:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,23 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
+from pytorch_lightning.loggers import WandbLogger
+
+import wandb
+
+hyperparameter_defaults = dict(
+    batch_size = 32,
+    learning_rate = 1e-3,
+    optimizer = 'adam',
+    epochs = 1
+)
+
+
+wandb.init(config=hyperparameter_defaults)
+
+config = wandb.config
+
+
 class obj(object):
     def __init__(self, d):
         for a, b in d.items():
@@ -32,6 +49,10 @@ class ScoreBasedDiffusion(pl.LightningModule):
 
         self.conf  = conf
         self.save_hyperparameters()
+
+        self.validation_step_outputs = []
+        self.test_step_outputs = []
+        self.training_step_outputs =[]
 
         self.model = UNet(input_channels= self.conf.model.in_channel,
                           input_height=self.conf.model.height,
@@ -56,8 +77,8 @@ class ScoreBasedDiffusion(pl.LightningModule):
 
 
     def configure_optimizers(self):
-        if self.conf.training.optimizer.type == 'adam':
-            optimizer = optim.Adam(self.model.parameters(), lr=self.conf.training.optimizer.lr)
+        if config.optimizer == 'adam':
+            optimizer = optim.Adam(self.model.parameters(), lr= config.learning_rate)
         else:
             raise NotImplementedError
 
@@ -77,8 +98,22 @@ class ScoreBasedDiffusion(pl.LightningModule):
         score = self.model(perturbed_x, random_t)
         loss = torch.mean(torch.sum((score * std[:, None, None, None] + z)**2, dim=(1,2,3)))
         tensorboard_logs = {'train_loss': loss}
+        self.log('train_loss', loss)
+
+        self.training_step_outputs.append({'train_loss': loss})# , 'bpd': bpd.sum() , 'bpd_items': bpd.shape[0]})
 
         return {'loss': loss, 'log': tensorboard_logs}
+
+
+    def on_train_epoch_end(self):
+      avg_loss         = torch.stack([x['train_loss'] for x in self.training_step_outputs]).mean()   
+      #all_bpd =torch.stack([x['bpd'] for x in self.training_step_outputs]).sum()
+
+      #all_items = torch.stack([torch.tensor(x['bpd_items']) for x in self.training_step_outputs]).sum()
+      self.log('avg_train_loss', avg_loss)
+      #wandb.log('avg_bpd', all_bpd/all_items)
+      
+      return {'avg_train_loss': avg_loss}
     
 
     def train_dataloader(self):
@@ -89,9 +124,12 @@ class ScoreBasedDiffusion(pl.LightningModule):
                                 num_workers=self.conf.training.dataloader.num_workers,
                                 pin_memory=True,
                                 drop_last=self.conf.training.dataloader.drop_last)
+        
+        print(len(train_loader))
 
         return train_loader
-    
+
+
     def val_dataloader(self):
         valid_loader = DataLoader(self.valid_set,
                                   batch_size=self.conf.validation.dataloader.batch_size,
@@ -113,10 +151,10 @@ class ScoreBasedDiffusion(pl.LightningModule):
         
         return test_loader
 
+    
     def validation_step(self, batch, batch_nb):
 
         eps = 1e-5
-
         x , _ = batch
 
         random_t = torch.rand(x.shape[0], device=x.device) * (1. - eps) + eps  
@@ -127,20 +165,29 @@ class ScoreBasedDiffusion(pl.LightningModule):
         loss = torch.mean(torch.sum((score * std[:, None, None, None] + z)**2, dim=(1,2,3)))
 
         x = (x * 255. + torch.rand_like(x)) / 256.    
-        _, bpd = ode_likelihood(x, self.model, self.sde.marginal_prob_std,
-                                self.sde.g, x.shape[0], eps)
+        _, bpd = ode_likelihood(x, self.model, x.shape[0], eps)
     
-        all_bpds += bpd.sum()
-        all_items += bpd.shape[0]
+        #all_bpds += bpd.sum()
+        #all_items += bpd.shape[0]
 
-        avg_bpd = all_bpds / all_items
+        #avg_bpd = bpd.sum() / bpd.shape[0]
+
+        self.log('val_loss', loss)
+
+        self.validation_step_outputs.append({'val_loss': loss , 'bpd': bpd.sum() , 'bpd_items': bpd.shape[0]})
     
-        return {'val_loss': loss , 'avg_bpd': avg_bpd}
+        return {'val_loss': loss}
     
 
-    def validation_epoch_end(self, outputs):
-        avg_loss         = torch.stack([x['val_loss'] for x in outputs]).mean()
-        tensorboard_logs = {'val_loss': avg_loss}
+    def on_validation_epoch_end(self):
+        avg_loss         = torch.stack([x['val_loss'] for x in self.validation_step_outputs]).mean()
+        
+        all_bpd =torch.stack([x['bpd'] for x in self.validation_step_outputs]).sum()
+
+        all_items = torch.stack([torch.tensor(x['bpd_items']) for x in self.validation_step_outputs]).sum()
+
+        #tensorboard_logs = {'val_loss': avg_loss , 'avg_bpd': all_bpd/all_items}
+
         sample_batch_size = 64
 
         samples = ode_sampler(self.model, sample_batch_size)
@@ -148,9 +195,15 @@ class ScoreBasedDiffusion(pl.LightningModule):
         samples = samples.clamp(0.0, 1.0)
 
         sample_grid = make_grid(samples, nrow=int(np.sqrt(sample_batch_size)))
-        self.logger.experiment.add_image(f'generated_images', sample_grid, self.current_epoch)
-        
-        return {'val_loss': avg_loss, 'log': tensorboard_logs}
+        wandb.log({'val_images': [wandb.Image(sample_grid, caption='MNIST')]})
+        wandb.log({'avg_bpd': all_bpd/all_items})
+
+
+        print('val_avg_bpd', all_bpd/all_items)
+
+        self.validation_step_outputs = []
+
+        return {'val_loss': avg_loss}
     
 
     def test_step(self, batch, batch_nb):
@@ -162,24 +215,32 @@ class ScoreBasedDiffusion(pl.LightningModule):
         samples = samples.clamp(0.0, 1.0)
 
         sample_grid = make_grid(samples, nrow=int(np.sqrt(sample_batch_size)))
-        self.logger.experiment.add_image(f'generated_images', sample_grid, self.current_epoch)
+        #self.logger.experiment.add_image(f'generated_images', sample_grid, self.current_epoch)
 
 
         x = (x * 255. + torch.rand_like(x)) / 256.    
-        _, bpd = ode_likelihood(x, self.model, self.sde.marginal_prob_std,
-                                self.sde.g,
+        _, bpd = ode_likelihood(x, self.model,
                                 x.shape[0], eps=1e-5)
+
+        self.log('testbpd', bpd)
     
-        all_bpds += bpd.sum()
-        all_items += bpd.shape[0]
+        self.test_step_outputs.append({'bpd': bpd.sum() , 'bpd_items': bpd.shape[0]})
 
-        avg_bpd = all_bpds / all_items
+        return {'bpd': bpd.sum() , 'bpd_items': bpd.shape[0]}
 
-        tensorboard_logs = {'avg_bpd': avg_bpd}
 
-        return {'avg_bpd': avg_bpd ,'log': tensorboard_logs}
+    def on_test_epoch_end(self):
+
+      all_bpd =torch.stack([x['bpd'] for x in self.test_step_outputs]).sum()
+      all_items = torch.stack([torch.tensor(x['bpd_items']) for x in self.test_step_outputs]).sum()
+      tensorboard_logs = {'test_avg_bpd': all_bpd/all_items}
+      self.log('test_avg_bpd', all_bpd/all_items)
+
+      return {'log': tensorboard_logs}
+ 
 
 if __name__ == "__main__":
+
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--train", action="store_true", default=False, help="Training or evaluation?")
@@ -193,6 +254,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     path_to_config = args.config
+
+
     with open(path_to_config, 'r') as f:
         conf = json.load(f)
 
@@ -200,41 +263,22 @@ if __name__ == "__main__":
 
     score_based_diffusion_model = ScoreBasedDiffusion(conf)
 
-    checkpoint_callback = ModelCheckpoint(filepath=os.path.join(args.ckpt_dir, 'sde_{epoch:02d}-{val_loss:.2f}'),
-                                            monitor='val_loss',
+    wandb_logger = WandbLogger()
+
+    checkpoint_callback = ModelCheckpoint(dirpath=os.path.join("/content/sample_data/ckptdir"),
+                                           filename="best",
+                                            monitor='avg_bpd',
+                                            mode = 'min', 
                                             verbose=False,
                                             save_last=True,
-                                            save_top_k=-1,
+                                            save_top_k=1,
                                             save_weights_only=True,
-                                            mode='auto',
-                                            period=args.ckpt_freq,
-                                            prefix='')
+                                            
+                                            )
     
-    trainer = pl.Trainer(fast_dev_run=False,
-                            gpus=args.n_gpu,
-                            max_steps=conf.training.n_iter,
-                            precision=conf.model.precision,
-                            gradient_clip_val=1.,
-                            progress_bar_refresh_rate=20,
-                            checkpoint_callback=checkpoint_callback)
+    trainer = pl.Trainer(fast_dev_run= False,logger=wandb_logger, accelerator ="gpu",devices=args.n_gpu, strategy="ddp", max_epochs= config.epochs,num_nodes= 1,check_val_every_n_epoch=10 ,callbacks=checkpoint_callback)
 
     trainer.fit(score_based_diffusion_model)
-
-    trainer.test()
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
 
 
 
